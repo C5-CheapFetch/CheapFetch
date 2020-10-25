7d8207cd048ea38a4938fa9b9c864f0165310bc9
garciaarmando <armandobfmv@gmail.com>
2020-10-25 16:19:41 -0600

    Images to the carroussel

---
+++ components/CarrousselShops/index.js
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+export const CarrousselShops = () => {
+  return (
+    <Container>
+      <h1>Search once, find everywhere</h1>
+
+      <h5>Amazon OK</h5>
+      <h5>Sam's Club OK</h5>
+      <h5>Walmart OK</h5>
+      <h5>Mercado Libre OK</h5>
+      <h5>Ebay OK</h5>
+    </Container>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,46 @@
-import React from 'react'
-import { useFetchApi } from '../hooks/useFetchApi'
+import React from "react";
+import { useFetchApi } from "../hooks/useFetchApi";
 
-import Layout from '../components/Layout'
-import Search from '../components/Search'
-import CardSquare from '../components/CardSquare'
-import CardVertical from '../components/CardVertical'
-import CardHorizontal from '../components/CardHorizontal'
-import CardInfo from '../components/CardInfo'
-import ListSimpleStore from '../components/ListSimpleStore'
-import ListCategories from '../components/ListCategories'
-import ListStores from '../components/ListStores'
-import GridCards from '../components/GridCards'
+import Layout from "../components/Layout";
+import { CarrousselShops } from "../components/CarrousselShops";
+import Search from "../components/Search";
+import CardSquare from "../components/CardSquare";
+import CardVertical from "../components/CardVertical";
+import CardHorizontal from "../components/CardHorizontal";
+import CardInfo from "../components/CardInfo";
+import ListSimpleStore from "../components/ListSimpleStore";
+import ListCategories from "../components/ListCategories";
+import ListStores from "../components/ListStores";
+import GridCards from "../components/GridCards";
 
 const Index = () => {
-
   //FETCH A LA API DE PRUEBA
-  const [itemList] = useFetchApi('https://apidjangoprueba.azurewebsites.net/api/items-list/')
+  const [itemList] = useFetchApi(
+    "https://apidjangoprueba.azurewebsites.net/api/items-list/"
+  );
 
-  
   //___local storage___
   // se guarda la llamada de la api en el local storage para usarse en toda la app
   // localStorage.setItem( "items", JSON.stringify( itemList ))
-  
+
   // se obtienen los productos del local storage y se almacenan en una variable
   // const itemsLocalStorage = JSON.parse( localStorage.getItem("items"))
-  
-  console.log(itemList)
 
+  console.log(itemList);
 
   return (
     <Layout>
-      <Search></Search>
-      <ListCategories></ListCategories>
-      <hr />
-      <ListSimpleStore></ListSimpleStore>
-      <hr />
-      <ListStores></ListStores>
-      <hr />
+      <Search> </Search> <CarrousselShops />
+      <ListCategories> </ListCategories> <hr />
+      <ListSimpleStore> </ListSimpleStore> <hr />
+      <ListStores> </ListStores> <hr />
       <CardSquare />
       <CardVertical />
       <CardHorizontal />
       <CardInfo />
       <GridCards />
     </Layout>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
