7ddf831c3452b73a34fee7d5b34fb023fc515b81
garciaarmando <armandobfmv@gmail.com>
2020-10-25 21:48:20 -0600

    Styles to images

--- components/CarrousselShops/index.js
+++ components/CarrousselShops/index.js
@@ -1,14 +1,139 @@
-import { Container } from "./styles";
+import { Container, ImagesContainer, StoreImage } from "./styles";
 export const CarrousselShops = () => {
   return (
     <Container>
       <h1>Search once, find everywhere</h1>
 
-      <h5>Amazon OK</h5>
-      <h5>Sam's Club OK</h5>
-      <h5>Walmart OK</h5>
-      <h5>Mercado Libre OK</h5>
-      <h5>Ebay OK</h5>
+      <ImagesContainer>
+        <StoreImage src="/images/AmazonCarroussel.png" alt="Amazon Logo" />
+
+        <StoreImage src="/images/EbayCarroussel.png" alt="Ebay Logo" />
+
+        <StoreImage
+          src="/images/MercadoLibreCarroussel.png"
+          alt="Mercado Libre Logo"
+        />
+
+        <StoreImage src="/images/SamsCarroussel.png" alt="Sam's Logo" />
+
+        <StoreImage src="/images/WalmartCarroussel.png" alt="Walmart Logo" />
+      </ImagesContainer>
     </Container>
+    //     <div class="slider">
+    //       <div class="slide-track">
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //         <div class="slide">
+    //           <img
+    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+    //             height="100"
+    //             width="250"
+    //             alt=""
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
   );
 };
