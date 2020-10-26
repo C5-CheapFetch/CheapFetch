38228c71175bce3c22d248269dc48e84f015eed3
garciaarmando <armandobfmv@gmail.com>
2020-10-26 00:04:19 -0600

    Starting with the animations

--- components/CarrousselShops/index.js
+++ components/CarrousselShops/index.js
@@ -1,139 +1,62 @@
-import { Container, ImagesContainer, StoreImage } from "./styles";
+// import { Container, ImagesContainer, StoreImage } from "./styles";
+import { Slider, SlideTrack, Slide } from "./styles";
+
 export const CarrousselShops = () => {
   return (
-    <Container>
-      <h1>Search once, find everywhere</h1>
+    <Slider>
+      <SlideTrack>
+        <Slide>
+          <img
+            // /images/nombre_img.png
+            src="/images/AliExpressCarroussel.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </Slide>
+        <Slide>
+          <img
+            src="/images/AmazonCarroussel.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </Slide>
+        <Slide>
+          <img
+            src="/images/EbayCarroussel.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </Slide>
+        <Slide>
+          <img
+            src="/images/MercadoLibreCarroussel.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </Slide>
+        <Slide>
+          <img
+            src="/images/SamsCarroussel.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </Slide>
+        <Slide>
+          <img
+            src="/images/WalmartCarroussel.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </Slide>
+      </SlideTrack>
+    </Slider>
 
-      <ImagesContainer>
-        <StoreImage src="/images/AmazonCarroussel.png" alt="Amazon Logo" />
-
-        <StoreImage src="/images/EbayCarroussel.png" alt="Ebay Logo" />
-
-        <StoreImage
-          src="/images/MercadoLibreCarroussel.png"
-          alt="Mercado Libre Logo"
-        />
-
-        <StoreImage src="/images/SamsCarroussel.png" alt="Sam's Logo" />
-
-        <StoreImage src="/images/WalmartCarroussel.png" alt="Walmart Logo" />
-      </ImagesContainer>
-    </Container>
-    //     <div class="slider">
-    //       <div class="slide-track">
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //         <div class="slide">
-    //           <img
-    //             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
-    //             height="100"
-    //             width="250"
-    //             alt=""
-    //           />
-    //         </div>
-    //       </div>
-    //     </div>
+    //   <h1>Search once, find everywhere</h1>
   );
 };
