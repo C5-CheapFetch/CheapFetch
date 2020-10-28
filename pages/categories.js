38b993f12fb476bc4d45cb08bc2f5e12d0325866
garciaarmando <armandobfmv@gmail.com>
2020-10-28 00:29:08 -0600

    A different title for each page

--- pages/categories.js
+++ pages/categories.js
@@ -1,8 +1,12 @@
 import Layout from "../components/Layout";
+import Head from "next/head";
 import ListCategories from "../components/ListCategories";
 const Categories = () => {
   return (
     <Layout>
+      <Head>
+        <title>CheapFetch - Categories</title>
+      </Head>
       <ListCategories />
     </Layout>
   );
--- pages/stores.js
+++ pages/stores.js
@@ -1,8 +1,12 @@
 import Layout from "../components/Layout";
+import Head from "next/head";
 import ListSimpleStore from "../components/ListSimpleStore";
 const Stores = () => {
   return (
     <Layout>
+      <Head>
+        <title>CheapFetch - Stores</title>
+      </Head>
       <ListSimpleStore />
     </Layout>
   );
