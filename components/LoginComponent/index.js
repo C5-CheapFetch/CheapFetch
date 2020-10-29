38b7c8a8920bbc4df23b5243e7ca431193a7bac9
Nohora <nohorapatriciam@icloud.com>
2020-10-29 04:41:02 -0500

    styles login

--- components/LoginComponent/index.js
+++ components/LoginComponent/index.js
@@ -1,52 +1,78 @@
 import React from "react";
+import {
+  Header,
+  Header__img,
+  Login,
+  Login__container,
+  Login__container_form,
+  Login__container__remember_me,
+  Login__container__remember_me_a,
+  Login__container__social_media,
+  Login__social_media__img,
+  Login__container__register,
+  Login__container__register_a,
+  Input,
+  Button,
+  Footer,
+  Footer__a,
+} from "./styles";
 
 const LoginComponent = () => {
   return (
     <div>
-      <header className="header">
-        <img
-          className="header__img"
-          src="public/images/alfa-romeo.svg"
-          alt="logo"
-          width="50px"
-        />
-      </header>
-      <section className="login">
-        <section className="login__container">
+      <Header>
+        <Header__img src="images/alfa-romeo.svg" alt="logo" width="50px" />
+      </Header>
+      <Login>
+        <Login__container>
           <h2>Inicia Sesion</h2>
-          <form className="login__container-form">
-            <input className="input" type="text" placeholder="Correo" />
-            <input className="input" type="password" placeholder="Contraseña" />
-            <button className="button">Iniciar Sesion</button>
-            <div className="login__container--remember-me">
+          <Login__container_form>
+            <Input type="text" placeholder="Correo" />
+            <Input className="input" type="password" placeholder="Contraseña" />
+            <Button>Iniciar Sesion</Button>
+            <Login__container__remember_me>
               <label>
-                <input type="checkbox" id="box1" defaultValue="checkbox" />
+                <Input type="checkbox" id="box1" defaultValue="checkbox" />
                 Recuerdame
               </label>
-              <a href="http://">Olvidé mi Contraseña</a>
-            </div>
-          </form>
-          <section className="login__container--social-media">
+              <Login__container__remember_me_a href="http://">
+                Olvidé mi Contraseña
+              </Login__container__remember_me_a>
+            </Login__container__remember_me>
+          </Login__container_form>
+
+          <Login__container__social_media>
             <div>
-              <img src="public/images/icons8-twitter-48.png" alt="twitter" />
+              <Login__social_media__img
+                src="images/icons8-twitter-48.png"
+                alt="twitter"
+              />
               Inicia Sesión con Twitter
             </div>
             <div>
-              <img src="public/images/icons8-google-48.png" alt="google" />
+              <Login__social_media__img
+                src="images/icons8-google-48.png"
+                alt="google"
+              />
               Inicia sesión con Google
             </div>
-          </section>
-          <p className="login__container--register">
+          </Login__container__social_media>
+
+          <Login__container__register>
             No tienes ninguna cuenta
-            <a href="register.html">Regístrate</a>
-          </p>
-        </section>
-      </section>
-      <footer className="footer">
-        <a href="/">Términos de Uso</a>
-        <a href="/">Declaración de Privacidad</a>
-        <a href="/">Centro de Ayuda</a>
-      </footer>
+            <Login__container__register_a>
+              {" "}
+              Regístrate{" "}
+            </Login__container__register_a>
+          </Login__container__register>
+        </Login__container>
+      </Login>
+
+      <Footer>
+        <Footer__a>Términos de Uso</Footer__a>
+        <Footer__a>Declaración de Privacidad</Footer__a>
+        <Footer__a>Centro de Ayuda</Footer__a>
+      </Footer>
     </div>
   );
 };
---
+++ components/LoginComponent/styles.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 0;
+  background-color: #fac5c5;
+`;
+
+export const Header__img = styled.img`
+  width: 100px;
+  margin-left: 30px;
+`;
+
+export const Login = styled.div`
+  background: linear-gradient(#fac5c5, #0000);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0px 30px;
+  min-height: calc(100vh - 200px);
+`;
+
+export const Login__container = styled.section`
+  border: 2px solid #fac5c5;
+  border-radius: 40px;
+  color: #1c1c1c;
+  padding: 60px 68px 40px;
+  min-height: 700px;
+  width: 300px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  box-shadow: 0px 0px 5px 5px #1c1c1c;
+`;
+
+export const Login__container_form = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  color: #1c1c1c;
+`;
+
+export const Login__container__remember_me = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  color: #1c1c1c;
+`;
+
+export const Login__container__social_media = styled.div`
+  align-items: center;
+  font-size: 14px;
+  margin-bottom: 10px;
+`;
+
+export const Login__container__register = styled.p`
+  font-size: 14px;
+`;
+
+export const Login__container__register_a = styled.a`
+  color: #1c1c1c;
+  font-weight: bold;
+  font-size: 16px;
+  text-decoration: none;
+`;
+
+export const Input = styled.input`
+  background-color: transparent;
+  border-left: 0px;
+  border-top: 0px;
+  border-right: 0px;
+  border-bottom: 2px solid #1c1c1c;
+  margin-bottom: 20px;
+  padding: 20px 0px;
+  outline: none;
+`;
+
+export const Button = styled.button`
+  background-color: #1c1c1c;
+  color: white;
+  height: 40px;
+  cursor: pointer;
+  font-weight: bold;
+  letter-spacing: 0.5px;
+  margin: 10px 0px;
+`; 
+
+export const Login__social_media__img = styled.img`
+  width: 30px;
+  margin-right: 10px;
+`; 
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  width: 100%;
+`;
+
+export const Footer__a = styled.a`
+  text-decoration: none;
+  color: #1c1c1c;
+  cursor: pointer;
+  font-size: 14px;
+  padding-left: 30px;
+`;
+
+export const Login__container__remember_me_a = styled.a`
+  color: #1c1c1c;
+  font-weight: bold;
+  font-size: 16px;
+  text-decoration: none;
+`;
+
+//** 
+//@media only screen and (max-width: 600px) {
+  //.login__container {
+    //background-color: transparent;
+    //padding: 15px;
+    //width: 100%;
+  //}
+
+  //.footer {
+    //align-items: flex-start;
+    //flex-direction: column;
+  //}
+//}
+
+
