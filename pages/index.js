d4456ded674ea9a1d47e22697a8aba046302c380
garciaarmando <armandobfmv@gmail.com>
2020-10-30 20:52:21 -0600

    API problem fixed

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ import GridCards from "../components/GridCards";
 
 const Index = () => {
   //FETCH A LA API DE PRUEBA
-  // const [itemList] = useFetchApi(
-  //   "https://apidjangoprueba.azurewebsites.net/api/items-list/"
-  // );
+  const [itemList] = useFetchApi(
+    "https://apidjangoprueba.azurewebsites.net/api/items-list/"
+  );
 
   //___local storage___
   // se guarda la llamada de la api en el local storage para usarse en toda la app
@@ -20,7 +20,7 @@ const Index = () => {
   // se obtienen los productos del local storage y se almacenan en una variable
   // const itemsLocalStorage = JSON.parse( localStorage.getItem("items"))
 
-  // console.log(itemList);
+  console.log(itemList);
 
   return (
     <Layout>
