0a494fd2e517bb6acedd707fedcad357277c84aa
garciaarmando <armandobfmv@gmail.com>
2020-10-31 15:03:32 -0600

    typo on pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,16 @@ import SearchHome from "../components/SearchHome";
 import GridCards from "../components/GridCards";
 
 const Index = () => {
-
-
   return (
     <Layout>
       <Head>
-<<<<<<< HEAD
         <title> CheapFetch - Home </title>{" "}
       </Head>{" "}
-      <Search />
-=======
-        <title>CheapFetch - Home</title>
-      </Head>
       <SearchHome />
->>>>>>> f5ab9f40ea05d9de10396e81e9631107431c0c7d
       <CarrousselShops />
       <GridCards />
     </Layout>
   );
 };
-
 
 export default Index;
