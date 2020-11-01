628eb64d2fe16c39f229f5c5a1b91740677ad2db
garciaarmando <armandobfmv@gmail.com>
2020-11-01 17:53:42 -0600

    Finishing the login-logout process with dinamics menus

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,54 +4,54 @@ import { Footer, PlatziCheap, Social, Text } from "./styles";
 
 const Footers = () => {
   return (
-      <Footer>
-        <PlatziCheap>
-          <p>2020 CHEAPFETCH, con 💚 de Platzi Master 18</p>
-        </PlatziCheap>
-        <Text>
-          <p>About Us</p>
-          <p>Help</p>
-          <p>Follow Us</p>
-        </Text>
-        <Social>
-          <Link href="">
-            <a className="social_link twitter">
-              <img
-                src="images/icons8-twitter.svg"
-                height={50}
-                alt="Logo de Twitter"
-              />
-            </a>
-          </Link>
-          <Link href="">
-            <a className="social_link facebook">
-              <img
-                src="images/icons8-facebook.svg"
-                height={50}
-                alt="Logo de Facebook"
-              />
-            </a>
-          </Link>
-          <Link href="">
-            <a className="social_link tiktok">
-              <img
-                src="images/icons8-tik-tok.svg"
-                height={50}
-                alt="Logo de Tiktok"
-              />
-            </a>
-          </Link>
-          <Link href="">
-            <a href="/" className="social_link instagram">
-              <img
-                src="images/icons8-instagram.svg"
-                height={50}
-                alt="Logo de instagram"
-              />
-            </a>
-          </Link>
-        </Social>
-      </Footer>
+    <Footer>
+      <PlatziCheap>
+        <p>2020 CHEAPFETCH | Hecho con 💚 en Platzi Master | C5</p>
+      </PlatziCheap>
+      <Text>
+        <p>About Us</p>
+        <p>Help</p>
+        <p>Follow Us</p>
+      </Text>
+      <Social>
+        <Link href="">
+          <a className="social_link twitter">
+            <img
+              src="images/icons8-twitter.svg"
+              height={50}
+              alt="Logo de Twitter"
+            />
+          </a>
+        </Link>
+        <Link href="">
+          <a className="social_link facebook">
+            <img
+              src="images/icons8-facebook.svg"
+              height={50}
+              alt="Logo de Facebook"
+            />
+          </a>
+        </Link>
+        <Link href="">
+          <a className="social_link tiktok">
+            <img
+              src="images/icons8-tik-tok.svg"
+              height={50}
+              alt="Logo de Tiktok"
+            />
+          </a>
+        </Link>
+        <Link href="">
+          <a href="/" className="social_link instagram">
+            <img
+              src="images/icons8-instagram.svg"
+              height={50}
+              alt="Logo de instagram"
+            />
+          </a>
+        </Link>
+      </Social>
+    </Footer>
   );
 };
 
--- components/RightNav/index.js
+++ components/RightNav/index.js
@@ -1,36 +1,86 @@
 import Link from "next/link";
 import { Anchor, List, Button, ButtonA, Li } from "./styles";
+import { useFetchUser } from "../../utils/user";
 
 const RightNav = () => {
+  const { user, loading } = useFetchUser();
   return (
-        <List>
-          <Li>
-            <Link href="/">
-              <Anchor> Home </Anchor>
-            </Link>
-          </Li>
-          <Li>
-            <Link href="/categories">
-              <Anchor> Categories </Anchor>
-            </Link>
-          </Li>
-          <Li>
-            <Link href="/stores">
-              <Anchor> Stores </Anchor>
-            </Link>
-          </Li>
-          <Li>
-            <Button href="/stores">
-              <ButtonA> Registrarse </ButtonA>
-            </Button>
-          </Li>
-          <Li>
-            <Button href="/">
-              <ButtonA> Inicio Sesion</ButtonA>
-            </Button>
-          </Li>
-        </List>
+    <List>
+      <Li>
+        <Link href="/">
+          <Anchor> Home </Anchor>
+        </Link>
+      </Li>
+      <Li>
+        <Link href="/categories">
+          <Anchor> Categories </Anchor>
+        </Link>
+      </Li>
+      <Li>
+        <Link href="/stores">
+          <Anchor> Stores </Anchor>
+        </Link>
+      </Li>
+      {user && !loading
+        ? [
+            <Li key="/api/logout">
+              <Link href="/api/logout">
+                <Anchor> Log Out </Anchor>
+              </Link>
+            </Li>,
+            <Li key="/api/me">
+              <Link href="/api/me">
+                <Anchor> Profile </Anchor>
+              </Link>
+            </Li>,
+          ]
+        : null}
+      {!user && !loading ? (
+        <Li key="/api/login">
+          <Link href="/api/login">
+            <Anchor> Log In </Anchor>
+          </Link>
+        </Li>
+      ) : null}
+    </List>
   );
 };
 
 export default RightNav;
+
+// const Navbar = () => {
+//   const { user, loading } = useFetchUser();
+//   // console.log(user, loading);
+//   return (
+//     <StyledHeader>
+//       <Menu mode="horizontal">
+//         <Menu.Item key="/">
+//           <Link href="/">
+//             <a>Home</a>
+//           </Link>
+//         </Menu.Item>
+//         {user && !loading
+//           ? [
+//               <Li key="/api/logout">
+//                 <Link href="/api/logout">
+//                   <Anchor> Log Out </Anchor>
+//                 </Link>
+//               </Li>,
+//               <Li key="/api/me">
+//                 <Link href="/api/me">
+//                   <Anchor> Profile </Anchor>
+//                 </Link>
+//               </Li>,
+//             ]
+//           : null}
+//         {!user && !loading ? (
+//           <Li key="/api/login">
+//             <Link href="/api/login">
+//               <Anchor> Log In </Anchor>
+//             </Link>
+//           </Li>
+//         ) : null}
+//       </Menu>
+//     </StyledHeader>
+//   );
+// };
