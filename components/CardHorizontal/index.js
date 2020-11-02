a016fc769831971f6352df39d667b6d13565d01f
Gerardo Garavito <garavitogerardo@gmail.com>
2020-11-01 21:19:02 -0600

    created the SelectCategory component

--- components/CardHorizontal/index.js
+++ components/CardHorizontal/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import {Card, Image} from './styles'
+import {Card, Image, Url} from './styles'
 
 const CardHorizontal = ({picture_url, url_located}) => {
 
   return(
-    <a href={url_located} target='_blank'>
+    <Url href={url_located} target='_blank'>
       <Card>
         <Image src={picture_url} />
       </Card>
-    </a>
+    </Url>
   )
 }
 
