1d73368fd7fe961d5078e87b86255b08e71b074c
Gerardo Garavito <garavitogerardo@gmail.com>
2020-11-04 15:53:52 -0600

    fixed some errors and added the .env file to my branch

--- pages/search/[...text].js
+++ pages/search/[...text].js
@@ -4,11 +4,7 @@ import SearchBar from "../../components/SearchBar";
 import ListCardInfo from "../../components/ListCardInfo";
 import { useRouter } from "next/router";
 
-<<<<<<< HEAD:pages/search/[...search].js
 const Search = () => {
-=======
-const Searchx = () => {
->>>>>>> 38ccc6fb7ef67ce493915179b01bb18619a6e58f:pages/search/[...text].js
   const router = useRouter();
   return (
     <Layout>
@@ -18,8 +14,4 @@ const Searchx = () => {
   );
 };
 
-<<<<<<< HEAD:pages/search/[...search].js
 export default Search;
-=======
-export default Searchx;
->>>>>>> 38ccc6fb7ef67ce493915179b01bb18619a6e58f:pages/search/[...text].js
