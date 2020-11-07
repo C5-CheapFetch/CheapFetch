b012101da709b78bf4f1072a7cf5ce8a8c3ad043
garciaarmando <armandobfmv@gmail.com>
2020-11-06 20:45:34 -0600

    Fixing a conflict

---
+++ pages/search/[...text].js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import SearchBar from "../../components/SearchBar";
+import ListCardInfo from "../../components/ListCardInfo";
+import { useRouter } from "next/router";
+
+const Search = () => {
+  const router = useRouter();
+  return (
+    <Layout>
+      <SearchBar />
+      <ListCardInfo search={router.query.text} />
+    </Layout>
+  );
+};
+
+export default Search;
