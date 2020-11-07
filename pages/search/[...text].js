5dfe57218f877ec675bc2db24f25a65b803e1515
garciaarmando <armandobfmv@gmail.com>
2020-11-06 20:42:51 -0600

    conflict fixed

--- pages/search/[...text].js
+++ pages/search/[...text].js
@@ -4,22 +4,14 @@ import SearchBar from "../../components/SearchBar";
 import ListCardInfo from "../../components/ListCardInfo";
 import { useRouter } from "next/router";
 
-<<<<<<< HEAD:pages/search/[...search].js
 const Search = () => {
-=======
-const Searchx = () => {
->>>>>>> 38ccc6fb7ef67ce493915179b01bb18619a6e58f:pages/search/[...text].js
   const router = useRouter();
   return (
     <Layout>
       <SearchBar />
-      <ListCardInfo search={router.query.text} />
+      <ListCardInfo search={router.query.text} />{" "}
     </Layout>
   );
 };
 
-<<<<<<< HEAD:pages/search/[...search].js
 export default Search;
-=======
-export default Searchx;
->>>>>>> 38ccc6fb7ef67ce493915179b01bb18619a6e58f:pages/search/[...text].js
