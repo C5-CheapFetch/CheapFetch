10afcdd9a730ccb87a76fc62a9b222344fd7294f
garciaarmando <armandobfmv@gmail.com>
2020-11-09 22:44:06 -0600

    latest changes from main

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,50 +1,48 @@
-import Link from 'next/link'
-import React from 'react'
-import { Footer, PlatziCheap, Social, Text, Img } from './styles'
+import Link from "next/link";
+import React from "react";
+import { Footer, PlatziCheap, Social, Text, Img } from "./styles";
 
 const Footers = () => {
   return (
     <Footer>
       <PlatziCheap>
-        <p>2020 CHEAPFETCH, con 💚 de Platzi Master 18</p>
-      </PlatziCheap>
+        <p> 2020 CheapFetch | Hecho con amor en Platzi Master💚 </p>
+      </PlatziCheap>{" "}
       <Social>
         <Text>
-          <p>About Us</p>
-          <p>Help</p>
-          <p>Follow Us</p>
-        </Text>
-        <Link href=''>
-          <a className='social_link twitter'>
-            <Img src='/images/twitter.svg' height={50} alt='Logo de Twitter' />
+          <p> About Us </p> <p> Help </p> <p> Follow Us </p>
+        </Text>{" "}
+        <Link href="">
+          <a className="social_link twitter">
+            <Img src="/images/twitter.svg" height={50} alt="Logo de Twitter" />
           </a>
         </Link>
-        <Link href=''>
-          <a className='social_link facebook'>
+        <Link href="">
+          <a className="social_link facebook">
             <Img
-              src='/images/facebook.svg'
+              src="/images/facebook.svg"
               height={50}
-              alt='Logo de Facebook'
+              alt="Logo de Facebook"
             />
           </a>
         </Link>
-        <Link href=''>
-          <a className='social_link tiktok'>
-            <Img src='/images/github.svg' height={50} alt='Logo de Tiktok' />
-          </a>
-        </Link>
-        <Link href=''>
-          <a href='/' className='social_link instagram'>
+        <Link href="">
+          <a className="social_link tiktok">
+            <Img src="/images/github.svg" height={50} alt="Logo de Tiktok" />
+          </a>{" "}
+        </Link>{" "}
+        <Link href="">
+          <a href="/" className="social_link instagram">
             <Img
-              src='/images/instagram.svg'
+              src="/images/instagram.svg"
               height={50}
-              alt='Logo de instagram'
+              alt="Logo de instagram"
             />
           </a>
         </Link>
       </Social>
     </Footer>
-  )
-}
+  );
+};
 
-export default Footers
+export default Footers;
