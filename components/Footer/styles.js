214106548bd8ded85766bc5e360361e9bbeabfe9
garciaarmando <armandobfmv@gmail.com>
2020-11-09 20:41:18 -0600

    latest changes from main

--- components/Footer/styles.js
+++ components/Footer/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Footer = styled.article`
+export const Footer = styled.article `
   background-color: #1c1c1c;
   height: 180px;
   color: lavender;
@@ -9,48 +9,41 @@ export const Footer = styled.article`
   justify-content: space-between;
   flex-wrap: wrap;
   padding: 10px;
-<<<<<<< HEAD
   margin-top: 100px;
-=======
->>>>>>> f221c3d69dd938accdaeb4f1c420a8b00aa0d631
 
-
-  @media (max-width: 767px){
+  @media (max-width: 767px) {
     display: block;
     text-align: center;
     border: 1px solid transparent;
-}
+  }
 `;
 
-export const PlatziCheap = styled.div`
-    background-color: #1C1C1C;
-    color: lavender;
-    display: flex;
-    align-items: space-between;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin: 5px;
+export const PlatziCheap = styled.div `
+  background-color: #1c1c1c;
+  color: lavender;
+  display: flex;
+  align-items: space-between;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 5px;
 `;
 
-export const Text = styled.div`
+export const Text = styled.div `
   margin: 10px;
   padding: 10px;
 `;
 
-export const Social = styled.div`
-    color: #f4fbff;
+export const Social = styled.div `
+  color: #f4fbff;
 
-    height: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin: 10px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 10px;
 `;
 
-export const Img = styled.img`
+export const Img = styled.img `
   margin-right: 8px;
-`
-
-
-
+`;
